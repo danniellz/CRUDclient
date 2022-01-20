fc5905ac61b14a4e378239d5f30677624c6e63c4
Daniel <daniel19brizuela@gmail.com>
2022-01-20 10:15:28 +0100

    control de los campos con limite de longitud, boton limpiar funcional

--- src/GESRE/controller/PiezaViewController.java
+++ src/GESRE/controller/PiezaViewController.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import static javafx.application.Application.launch;
 import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -24,10 +24,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -42,11 +41,15 @@ public class PiezaViewController {
     //LOGGER
     private static final Logger LOG = Logger.getLogger(PiezaViewController.class.getName());
 
+    //********LABELS********
+    @FXML
+    private Label messageLbl;
+
     //********TEXT FIELDS********
     @FXML
     private TextField txtNombre;
     @FXML
-    private TextField txtDescripcion;
+    private TextArea txtADescripcion;
     @FXML
     private TextField txtStock;
     @FXML
@@ -130,19 +133,17 @@ public class PiezaViewController {
             btnAnadir.setDisable(true);
             btnEditar.setDisable(true);
             btnBorrar.setDisable(true);
-            btnLimpiar.setDisable(true);
             btnBuscar.setDisable(true);
-            btnLimpiar.setDisable(true);
+            txtNombreFiltro.setDisable(true);
             stage.setOnCloseRequest(this::handleSalir);
 
             //Listeners de los TextField al cambiar el texto, textProperty
-            /*txtNombre.textProperty().addListener(this::handleValidarTexto);
-            txtDescripcion.textProperty().addListener(this::handleValidarTexto);
-            txtStock.textProperty().addListener(this::handleValidarTexto);
-            txtNombreFiltro.textProperty().addListener(this::handleValidarTexto);*/
-            //Definir columnas de la tabla trabajador
-            
-            //Establecer el valor que aparecera dentro de la celda
+            txtNombre.textProperty().addListener(this::limitNombreTextField);
+            txtADescripcion.textProperty().addListener(this::limitDescripcionTextField);
+            txtStock.textProperty().addListener(this::limitStockTextField);
+            txtNombreFiltro.textProperty().addListener(this::limitNombreFiltroTextField);
+
+            //Establecer el valor que aparecera dentro de cada celda
             nombreCl.setCellValueFactory(new PropertyValueFactory<>("nombre"));
             descripcionCl.setCellValueFactory(new PropertyValueFactory<>("descripcion"));
             stockCl.setCellValueFactory(new PropertyValueFactory<>("stock"));
@@ -152,73 +153,20 @@ public class PiezaViewController {
             tablaPiezas.setItems(datosPieza);
 
             //Listeners
-            //btnLimpiar.setOnAction(this::handleBtnLimpiar);
+            btnLimpiar.setOnAction(this::handleLimpiar);
             //btnAnadir.setOnAction(this::handleBtnAnadir);
             //btnModificar.setOnAction(this::handleBtnModificar);
             //btnEliminar.setOnAction(this::handleBtnEliminar);
             //btnBuscar.setOnAction(this::handleBtnBuscar);
-            //mnCerrarSesion.setOnAction(this::handleCerrarSesion);
             //cerrarSesion.setOnAction(this::handleCerrarSesion);
+            //btnGestionIncidencia.setOnAction(this::startIncidenciaViewTWindow);
             //salir.setOnAction(this::handleSalir);
-
             //Mostrar ventana (asincrona)
             stage.show();
         } catch (Exception e) {
             LOG.log(Level.SEVERE, "Error al inicializar Stage", e);
         }
 
-    }
-
-    /**
-     * Llamar a este método cerrará la ventana
-     *
-     * @param closeEvent evento de ventana
-     */
-    public void handleSalir(WindowEvent closeEvent) {
-        try {
-            LOG.info("Confirmando cierre de ventana...");
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setHeaderText("¿Estás seguro de querer Salir?");
-            alert.setTitle("Salir");
-            Optional<ButtonType> option = alert.showAndWait();
-            if (option.get() == ButtonType.OK) {
-                LOG.info("Cerrando...");
-                Platform.exit();
-            } else {
-                LOG.info("Cierre del programa cancelado");
-                //Cancel the event process
-                closeEvent.consume();
-            }
-        } catch (Exception e) {
-            LOG.log(Level.SEVERE, "Error al intentar cerrar la ventana", e);
-        }
-    }
-
-    /**
-     * Llamar a este método cerrará la sesíón actual para volver a la ventana de SignIn
-     *
-     * @param logOutEvent Log Out action event
-     */
-    public void handleCerrarSesion(ActionEvent logOutEvent) {
-        try {
-            //Pressing the LogOut option will show an Alert to confirm it
-            LOG.info("Log Out button clicked");
-            LOG.info("Confirm Log Out");
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setHeaderText("¿Are you sure you want to log out?");
-            alert.setTitle("Log Out");
-            Optional<ButtonType> option = alert.showAndWait();
-            if (option.get() == ButtonType.OK) {
-                LOG.info("Logging out...");
-                startSignInWindow(stage);
-            } else {
-                LOG.info("Log Out Canceled");
-                //Cancel the event process
-                logOutEvent.consume();
-            }
-        } catch (IOException ex) {
-            LOG.log(Level.SEVERE, "Log Out error", ex);
-        }
     }
 
     /**
@@ -245,20 +193,94 @@ public class PiezaViewController {
         }
 
     }
+    
+    /**
+     * Llamar a este método abrirá la ventana de IncidenciaViewT (Gestion de incidencias del trabajador)
+     *
+     * @param primaryStage objeto Stage (Ventana)
+     * @throws IOException salta una excepcion si la ventana de IncidenciaViewT falla en abrirse
+     */
+    /*private void startIncidenciaViewTWindow(Stage primaryStage) throws IOException {
+        try {
+            LOG.info("Abriendo ventana IncidenciaViewTWindow...");
+            //Carga el archivo FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GESRE/vistas/IncidenciaViewT.fxml"));
+            Parent root = (Parent) loader.load();
+            //Obtiene el controlador
+            IncidenciaCLViewController controlador = ((SignInController) loader.getController());
+            //Establece el Stage
+            controlador.setStage(primaryStage);
+            //Inicializa la ventana
+            controlador.initStage(root);
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, "Error al intentar abrir la ventana de SignUp", ex);
+        }
+
+    }*/
 
     /**
      * Llamar a este método limpiara todos los campos si están informados
      */
-    private void limpiar() {
+    private void handleLimpiar(ActionEvent limpiarEvent) {
         txtNombre.setText("");
-        txtDescripcion.setText("");
+        txtADescripcion.setText("");
         txtStock.setText("");
         txtNombreFiltro.setText("");
 
         btnAnadir.setDisable(true);
-        btnLimpiar.setDisable(true);
-
         txtNombre.requestFocus();
+    }
+    
+    /**
+     * Llamar a este método cerrará la ventana
+     *
+     * @param salirWindowEvent evento de ventana
+     */
+    public void handleSalir(WindowEvent salirWindowEvent) {
+        try {
+            LOG.info("Confirmando cierre de ventana...");
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setHeaderText("¿Estás seguro de querer Salir?");
+            alert.setTitle("Salir");
+            Optional<ButtonType> option = alert.showAndWait();
+            if (option.get() == ButtonType.OK) {
+                LOG.info("Cerrando...");
+                Platform.exit();
+            } else {
+                LOG.info("Cierre del programa cancelado");
+                //Cancel the event process
+                salirWindowEvent.consume();
+            }
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "Error al intentar cerrar la ventana", e);
+        }
+    }
+
+    /**
+     * Llamar a este método cerrará la sesíón actual para volver a la ventana de SignIn
+     *
+     * @param cerrarSesiontEvent Cerrar Sesion action event
+     */
+    public void handleCerrarSesion(ActionEvent cerrarSesiontEvent) {
+        try {
+            //Pressing the LogOut option will show an Alert to confirm it
+            LOG.info("Log Out button clicked");
+            LOG.info("Confirm Log Out");
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setHeaderText("¿Are you sure you want to log out?");
+            alert.setTitle("Log Out");
+            Optional<ButtonType> option = alert.showAndWait();
+            if (option.get() == ButtonType.OK) {
+                LOG.info("Logging out...");
+                startSignInWindow(stage);
+            } else {
+                LOG.info("Log Out Canceled");
+                //Cancel the event process
+                cerrarSesiontEvent.consume();
+            }
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, "Log Out error", ex);
+        }
     }
 
     /**
@@ -268,10 +290,133 @@ public class PiezaViewController {
      */
     private boolean comprobarCampoVacio() {
         boolean vacio = false;
-        if (txtNombre.getText().isEmpty() || txtDescripcion.getText().isEmpty() || txtStock.getText().isEmpty()) {
+        if (txtNombre.getText().isEmpty() || txtADescripcion.getText().isEmpty() || txtStock.getText().isEmpty()) {
             vacio = true;
         }
         return vacio;
+    }
+    
+
+    /**
+     * Llamar a este método establece el control del campo Nombre (textProperty())
+     *
+     * @param observable campo objetivo cuyo valor cambia
+     * @param oldValue valor previo al cambio
+     * @param newValue ultimo valor introducido
+     */
+    private void limitNombreTextField(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+        try {
+            //si un 26 caracter es introducido, se borra
+            if (txtNombre.getText().length() > 25) {
+                //Preparar mensaje del label
+                messageLbl.setText("Límite de 25 caracteres alcanzado");
+                //Borrar ultimo caracter introducido
+                txtNombre.deleteNextChar();
+                //Color del campo rojo para feedback visual
+                txtNombre.setStyle("-fx-border-color: #DC143C ; -fx-border-width: 1.5px ;");
+                //Activar label
+                messageLbl.setStyle("-fx-text-fill: #DC143C");
+                messageLbl.setVisible(true);
+                
+            } else {
+                //Mientras los caracteres introducidos sean menor a 26, se desactiva el label y el campo vuelve a su color normal
+                txtNombre.setStyle("-fx-border-color: White;");
+                messageLbl.setVisible(false);
+                messageLbl.setStyle("");
+            }
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Error Setting User field control", ex);
+        }
+    }
+    
+    /**
+     * Llamar a este método establece el control del campo Descripcion (textProperty())
+     *
+     * @param observable campo objetivo cuyo valor cambia
+     * @param oldValue valor previo al cambio
+     * @param newValue ultimo valor introducido
+     */
+    private void limitStockTextField(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+        try {
+            String cadena = txtStock.getText();
+            //si un 4 caracter es introducido, se borra
+            if (cadena.length() > 3) {
+                //Borrar ultimo caracter introducido
+                txtStock.deleteNextChar();
+            }
+            //Control empty spaces
+            if (cadena.contains(" ")) {
+                txtStock.setText(cadena.replaceAll(" ", ""));
+            }
+
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Error Setting User field control", ex);
+        }
+    }
+    
+    /**
+     * Llamar a este método establece el control del campo Stock (textProperty())
+     *
+     * @param observable campo objetivo cuyo valor cambia
+     * @param oldValue valor previo al cambio
+     * @param newValue ultimo valor introducido
+     */
+    private void limitDescripcionTextField(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+        try {
+            String cadena = txtADescripcion.getText();
+            //si un 256 caracter es introducido, se borra
+            if (cadena.length() > 255) {
+                messageLbl.setText("Límite de 255 caracteres alcanzado");
+                //Borrar ultimo caracter introducido
+                txtADescripcion.deleteNextChar();
+                //Color del campo rojo para feedback visual
+                txtADescripcion.setStyle("-fx-border-color: #DC143C ; -fx-border-width: 1.5px ;");
+                //Activar label
+                messageLbl.setStyle("-fx-text-fill: #DC143C");
+                messageLbl.setVisible(true);
+                
+            } else {
+                //Mientras los caracteres introducidos sean menor a 26, se desactiva el label y el campo vuelve a su color normal
+                txtADescripcion.setStyle("-fx-border-color: White;");
+                messageLbl.setVisible(false);
+                messageLbl.setStyle("");
+            }
+
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Error Setting User field control", ex);
+        }
+    }
+    
+    /**
+     * Llamar a este método establece el control del campo NombreFiltro (textProperty())
+     *
+     * @param observable campo objetivo cuyo valor cambia
+     * @param oldValue valor previo al cambio
+     * @param newValue ultimo valor introducido
+     */
+    private void limitNombreFiltroTextField(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+        try {
+            //si un 26 caracter es introducido, se borra
+            if (txtNombreFiltro.getText().length() > 25) {
+                //Preparar mensaje del label
+                messageLbl.setText("Límite de 25 caracteres alcanzado");
+                //Borrar ultimo caracter introducido
+                txtNombreFiltro.deleteNextChar();
+                //Color del campo rojo para feedback visual
+                txtNombreFiltro.setStyle("-fx-border-color: #DC143C ; -fx-border-width: 1.5px ;");
+                //Activar label
+                messageLbl.setStyle("-fx-text-fill: #DC143C");
+                messageLbl.setVisible(true);
+                
+            } else {
+                //Mientras los caracteres introducidos sean menor a 26, se desactiva el label y el campo vuelve a su color normal
+                txtNombreFiltro.setStyle("-fx-border-color: White;");
+                messageLbl.setVisible(false);
+                messageLbl.setStyle("");
+            }
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Error Setting User field control", ex);
+        }
     }
 
 }
